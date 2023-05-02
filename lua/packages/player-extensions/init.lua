135f09fb6d620f640b16172b95ce336db4a4ebba
PrikolMen <olegis019@gmail.com>
2023-05-02 21:04:37 +0400

    Updated

--- lua/packages/player-extensions/init.lua
+++ lua/packages/player-extensions/init.lua
@@ -1,5 +1,5 @@
-import( gpm.LuaPackageExists( "packages/glua-extensions" ) and "packages/glua-extensions" or "https://raw.githubusercontent.com/Pika-Software/glua-extensions/main/package.json" )
-import( gpm.LuaPackageExists( "packages/nw3-vars" ) and "packages/nw3-vars" or "https://raw.githubusercontent.com/Pika-Software/nw3-vars/master/package.json" )
+import( gpm.LuaPackageExists( "packages/glua-extensions" ) and "packages/glua-extensions" or "https://github.com/Pika-Software/glua-extensions" )
+import( gpm.LuaPackageExists( "packages/nw3-vars" ) and "packages/nw3-vars" or "https://github.com/Pika-Software/nw3-vars" )
 
 local packageName = gpm.Package:GetIdentifier()
 local ArgAssert = ArgAssert
